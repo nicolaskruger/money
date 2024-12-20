002f1da63497adb93da4fec497b7296514b459e4
nicolas kruger <nicolas.nkruger@gmail.com>
2024-12-20 07:55:18 -0300

    create use controler

--- money/Controllers/UserController.cs
+++ money/Controllers/UserController.cs
@@ -1,6 +1,15 @@
-﻿namespace money.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace money.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class UserController
     {
+        [HttpGet(Name = "userById")]
+        public bool userById(int id)
+        {
+            return true;
+        }
     }
 }
