38fdf48537982a767c7e73038c7d48950d8db2fe
nicolas kruger <nicolas.nkruger@gmail.com>
2024-12-20 13:11:25 -0300

    user dto

--- money/Controllers/UserController.cs
+++ money/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using money.Dto;
 using System.Net;
 
 namespace money.Controllers
@@ -9,13 +10,8 @@ namespace money.Controllers
     public class UserController
     {
         [HttpPost(Name = "create user")]
-        public IActionResult createUser() {
-            return new JsonResult(new {}) { StatusCode = 201 };
-        }
-        [HttpGet(Name = "userById")]
-        public bool userById(int id)
-        {
-            return true;
+        public IActionResult CreateUser([FromBody] CreateUserDto User) {
+            return new JsonResult(User) { StatusCode = 201 };
         }
     }
 }
---
+++ money/Dto/CreateUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace money.Dto
+{
+    public class CreateUserDto
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+
+    }
+}
