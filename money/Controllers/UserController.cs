78f7bd10d0c6dd167fb1cd973f9bed88ed808482
nicolas kruger <nicolas.nkruger@gmail.com>
2024-12-20 14:41:38 -0300

    add swagger documentation

--- money/Controllers/UserController.cs
+++ money/Controllers/UserController.cs
@@ -1,16 +1,35 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using money.Dto;
-using System.Net;
 
 namespace money.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class UserController
+    public class UserController: Controller
     {
+        /// <summary>
+        /// Create User 
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "name": "Nícolas Krüger",
+        ///        "email": "sample@email.com",
+        ///        "password": "password1"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns the newly created user</response>
+        /// <response code="400">If the user is missing atributes</response>
         [HttpPost(Name = "create user")]
         public IActionResult CreateUser([FromBody] CreateUserDto User) {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             return new JsonResult(User) { StatusCode = 201 };
         }
     }
--- money/Dto/CreateUserDto.cs
+++ money/Dto/CreateUserDto.cs
@@ -1,9 +1,32 @@
-﻿namespace money.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace money.Dto
 {
+    /// <summary>
+    /// create use dto
+    /// </summary>
     public class CreateUserDto
     {
+        /// <summary>
+        /// name
+        /// </summary>
+        /// <example>Nícolas Krüger</example>
+        [Required(ErrorMessage = "name required")]
+        [Length(5, 100, ErrorMessage = "name to need to be more than 5 or less than 100")]
         public String Name { get; set; }
+        /// <summary>
+        /// email
+        /// </summary>
+        /// <example>sample@email.com</example>
+        [Required(ErrorMessage = "email required")]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "not valid email")]
         public String Email { get; set; }
+        /// <summary>
+        /// password
+        /// </summary>
+        /// <example>password1</example>
+        [Required(ErrorMessage = "password required")]
+        [RegularExpression(@"^(?=.*\d).{8,}$", ErrorMessage = "password must have 8 length and one digit")]
         public String Password { get; set; }
 
     }
